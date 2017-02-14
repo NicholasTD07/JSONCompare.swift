4dda4c876d72a137c1dbe6ee363e9d276c1a1d0c
NicholasTD07 <Nicholas.TD07@gmail.com>
2017-02-14 21:46:47 +1100

    Add another test to test comparing JSON.object

--- Tests/JSONCompareTests/JSONCompareTests.swift
+++ Tests/JSONCompareTests/JSONCompareTests.swift
@@ -2,12 +2,24 @@ import XCTest
 import JSONCompare
 
 class JSONCompareTests: XCTestCase {
-    func testComparingArray() {
-        let jsonValue = json(fromFile: "root_array")!
-
-        XCTAssertNotNil(jsonValue)
+    func testComparingObject() {
         XCTAssertEqual(
-            JSON(jsonValue),
+            JSON(json(fromFile: "root_object")!),
+            .object([
+                "title": .string("JSONCompare"),
+                "isOpensource": .bool(true),
+                "stars": .number(999),
+                "versions": .array([
+                    .string("v1.0.0: Equatable"),
+                    .string("v2.0.0: Diff"),
+                ])
+            ])
+        )
+    }
+
+    func testComparingArray() {
+        XCTAssertEqual(
+            JSON(json(fromFile: "root_array")!),
             .array([
                 .string("a"),
                 .string("b"),
