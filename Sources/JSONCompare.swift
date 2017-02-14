adc6bdd8296c7ce37a6c45d0bf7df2e0b7bfdd57
NicholasTD07 <Nicholas.TD07@gmail.com>
2017-02-14 21:44:33 +1100

    Complete `==` for the added data types in enum JSON

--- Sources/JSONCompare.swift
+++ Sources/JSONCompare.swift
@@ -1,8 +1,11 @@
 extension JSON: Equatable {
     public static func == (l: JSON, r: JSON) -> Bool {
         switch (l, r) {
-        case let (.string(l), .string(r)): return l == r
         case let (.array(l), .array(r)): return l == r
+        case let (.object(l), .object(r)): return l == r
+        case let (.string(l), .string(r)): return l == r
+        case let (.number(l), .number(r)): return l == r
+        case let (.bool(l), .bool(r)): return l == r
         case (.null, .null): return true
         default: return false
         }
