ca962504024a3f37f326cff24ed0fff1b7454170
NicholasTD07 <Nicholas.TD07@gmail.com>
2017-02-14 21:02:01 +1100

    Add `==` for basic definition of enum JSON

--- Sources/JSONCompare.swift
+++ Sources/JSONCompare.swift
@@ -1,4 +1,10 @@
-struct JSONCompare {
-
-    var text = "Hello, World!"
+extension JSON: Equatable {
+    public static func == (l: JSON, r: JSON) -> Bool {
+        switch (l, r) {
+        case let (.string(l), .string(r)): return l == r
+        case let (.array(l), .array(r)): return l == r
+        case (.null, .null): return true
+        default: return false
+        }
+    }
 }
